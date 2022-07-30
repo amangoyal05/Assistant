7fa0a9f7cb2bd631c0b8dc6ef0c13ee4f0278c89
Aman Goyal <aman.goyal16sms@gmail.com>
2022-07-30 09:20:43 +0530

    Added Time

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import speech_recognition as sr
 import pyttsx3
-#time
 from datetime import datetime
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-engine = pyttsx3.init()
-recording = sr.Recognizer()
 
+r = sr.Recognizer()
+#def Begin():
+#    engine = pyttsx3.init()
+#    engine.say("Good morning master. How may I help you?")
+#    engine.runAndWait()
+
+def SpeakText(command):
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+    
 with sr.Microphone() as source:
-    recording.adjust_for_ambient_noise(source)
-engine.say("Hello! How may I help you?")
-engine.runAndWait()
-audio = recording.listen(source)
-if audio == 'Time':
-    engine.say('The time is ' + current_time)
-
-try:
-    print("You said: n" + recording.recognize_google(audio))
-except Exception as e:
-    print(e)
+    r.adjust_for_ambient_noise(source, duration=0.2)
+    audio = r.listen(source)
+    MyText = r.recognize_google(audio,language="en-in")
+    MyText=MyText.lower()
+    print(MyText)
+            
+if(MyText=='time'):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    with sr.Microphone() as source1:
+        SpeakText("The time is " +current_time)
+        print("The time is ", current_time)
+else:
+    SpeakText("I am sorry I was unable to understand it. Can you please repeat it?")
