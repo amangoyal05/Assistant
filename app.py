f0c409c1a9384cd7b0866da2dddc1dfd55f08bb4
Aman Goyal <aman.goyal16sms@gmail.com>
2022-07-30 21:09:07 +0530

    Added Greetings

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import speech_recognition as sr
 import pyttsx3
 from datetime import datetime
-
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
 r = sr.Recognizer()
-#def Begin():
-#    engine = pyttsx3.init()
-#    engine.say("Good morning master. How may I help you?")
-#    engine.runAndWait()
+
+engine = pyttsx3.init()
+if('5:00:00'<current_time<'12:00:00'):
+    engine.say("Good morning master. How may I help you?")
+elif('12:00:01'<current_time<'17:00:00'):
+    engine.say("Good afternoon master. How may I help you?")
+elif('17:00:01'<current_time<'20:00:00'):
+    engine.say("Good evening master. How may I help you?")
+else:
+    engine.say("Yes master. How may I help you?")
+engine.runAndWait()
 
 def SpeakText(command):
     engine = pyttsx3.init()
     engine.say(command)
     engine.runAndWait()
-    
+
 with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source, duration=0.2)
     audio = r.listen(source)
@@ -21,10 +29,9 @@ with sr.Microphone() as source:
     print(MyText)
             
 if(MyText=='time'):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
     with sr.Microphone() as source1:
         SpeakText("The time is " +current_time)
         print("The time is ", current_time)
 else:
     SpeakText("I am sorry I was unable to understand it. Can you please repeat it?")
+    SpeakText()
