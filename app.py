5e080293a43f2e3467f866a15025a35029684a5a
Aman Goyal <aman.goyal16sms@gmail.com>
2022-07-31 15:04:42 +0530

    Weather Description

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import speech_recognition as sr
 import pyttsx3
 from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 r = sr.Recognizer()
@@ -24,14 +26,29 @@ def SpeakText(command):
 with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source, duration=0.2)
     audio = r.listen(source)
-    MyText = r.recognize_google(audio,language="en-in")
-    MyText=MyText.lower()
-    print(MyText)
+    MySpeech = r.recognize_google(audio,language="en-in")
+    MySpeech=MySpeech.lower()
+    print(MySpeech)
             
-if(MyText=='time'):
+if "time" in MySpeech:
     with sr.Microphone() as source1:
         SpeakText("The time is " +current_time)
         print("The time is ", current_time)
+elif "weather" and "today" in MySpeech:
+    with sr.Microphone() as source2:
+        SpeakText("Which city's weather do you want to know?")
+        city = r.listen(source2)
+        text = r.recognize_google(city)
+        url = "https://www.google.com/search?q="+"weather"+text
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'html.parser')
+        temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+        str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+        data = str.split('\n')
+        time = data[0]
+        sky = data[1]
+        SpeakText("Temperature is" + temp)
+        SpeakText("Time: " + time)
+        SpeakText("Sky Description: " + sky)
 else:
     SpeakText("I am sorry I was unable to understand it. Can you please repeat it?")
-    SpeakText()
