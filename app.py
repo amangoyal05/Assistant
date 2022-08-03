f1e2781d2ec64dfe7dde51c597ba837346d85716
Aman Goyal <aman.goyal16sms@gmail.com>
2022-08-03 09:50:35 +0530

    Added Wikipedia Search

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pyttsx3
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+import wikipedia
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 r = sr.Recognizer()
@@ -23,32 +24,46 @@ def SpeakText(command):
     engine.say(command)
     engine.runAndWait()
 
+def search(query):         
+    query = query.split(' ') 
+    query = " ".join(query[0:])
+    SpeakText("I am searching for " + query)
+    print(wikipedia.summary(query, sentences = 3))
+    SpeakText(wikipedia.summary(query, sentences = 3))
+
 with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source, duration=0.2)
-    audio = r.listen(source)
-    MySpeech = r.recognize_google(audio,language="en-in")
-    MySpeech=MySpeech.lower()
-    print(MySpeech)
+    while True:
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        audio = r.listen(source)
+        MySpeech = r.recognize_google(audio,language="en-in")
+        MySpeech=MySpeech.lower()
+        print(MySpeech)
             
-if "time" in MySpeech:
-    with sr.Microphone() as source1:
-        SpeakText("The time is " +current_time)
-        print("The time is ", current_time)
-elif "weather" and "today" in MySpeech:
-    with sr.Microphone() as source2:
-        SpeakText("Which city's weather do you want to know?")
-        city = r.listen(source2)
-        text = r.recognize_google(city)
-        url = "https://www.google.com/search?q="+"weather"+text
-        html = requests.get(url).content
-        soup = BeautifulSoup(html, 'html.parser')
-        temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
-        str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
-        data = str.split('\n')
-        time = data[0]
-        sky = data[1]
-        SpeakText("Temperature is" + temp)
-        SpeakText("Time: " + time)
-        SpeakText("Sky Description: " + sky)
-else:
-    SpeakText("I am sorry I was unable to understand it. Can you please repeat it?")
+        if "time" in MySpeech:
+            with sr.Microphone() as source1:
+                SpeakText("The time is " +current_time)
+                print("The time is ", current_time)
+        elif "weather" and "today" in MySpeech:
+            with sr.Microphone() as source2:
+                SpeakText("Which city's weather do you want to know?")
+                city = r.listen(source2)
+                text = r.recognize_google(city)
+                url = "https://www.google.com/search?q="+"weather"+text
+                html = requests.get(url).content
+                soup = BeautifulSoup(html, 'html.parser')
+                temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+                str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+                data = str.split('\n')
+                time = data[0]
+                sky = data[1]
+                SpeakText("Temperature is" + temp)
+                SpeakText("Time: " + time)
+                SpeakText("Sky Description: " + sky)
+        elif "search":
+            speech = r.recognize_google(audio)
+            search(speech)
+        elif "exit":
+            break
+        else:
+            SpeakText("I am sorry I was unable to understand it. Can you please repeat it?")
+            search(audio)
